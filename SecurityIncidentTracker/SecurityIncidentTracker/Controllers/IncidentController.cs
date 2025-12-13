beb5733bd9e2ca58c3353f6a8844ffc82d95eed9
laikaSec <bf1535ms@gmail.com>
2025-12-12 19:47:25 -0500

    Implement Incident Create [HttpGet] action to load dropdown data

--- SecurityIncidentTracker/SecurityIncidentTracker/Controllers/IncidentController.cs
+++ SecurityIncidentTracker/SecurityIncidentTracker/Controllers/IncidentController.cs
@@ -16,12 +16,23 @@ namespace SecurityIncidentTracker.Controllers
             _incidentService = incidentService;
         }
 
-        // Placeholder for the Create page (GET request)
-        // This will eventually show the blank form.
+        // GET: Incident/Create
+        // Shows the blank form to create a new incident. 
         [HttpGet]
         public async Task<IActionResult> Create()
         {
-            // TODO: Load dropdown data (types, responders) and show the view
+            // 1. Get the list of incident types for the dropdown
+            var types = await _incidentService.GetIncidentTypesAsync();
+
+            // 2. Get the list of responders for the dropdown
+            var responders = await _incidentService.GetRespondersAsync();
+
+            // 3. Pass these lists to the view using ViewBag
+            // ViewBag is a dynamic container for data the view needs but isn't part of the main model.
+            ViewBag.IncidentTypes = types;
+            ViewBag.Responders = responders;
+
+            // 4. Return the Create view
             return View();
         }
     }
