9410207782c9286baa2d35c5d7ebe17af6a25b25
laikaSec <bf1535ms@gmail.com>
2025-12-12 19:30:58 -0500

    Add IncidentController skeleton with service injection

---
+++ SecurityIncidentTracker/SecurityIncidentTracker/Controllers/IncidentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecurityIncidentTracker.Models;
+using SecurityIncidentTracker.Services;
+
+namespace SecurityIncidentTracker.Controllers
+{
+    // This controller manages incident-specific actions like creating, viewing details, and editing.
+    public class IncidentController : Controller
+    {
+        // Reference to our data service
+        private readonly IncidentService _incidentService;
+
+        // Constructor: inject the IncidentService so we can talk to the database
+        public IncidentController(IncidentService incidentService)
+        {
+            _incidentService = incidentService;
+        }
+
+        // Placeholder for the Create page (GET request)
+        // This will eventually show the blank form.
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            // TODO: Load dropdown data (types, responders) and show the view
+            return View();
+        }
+    }
+}
